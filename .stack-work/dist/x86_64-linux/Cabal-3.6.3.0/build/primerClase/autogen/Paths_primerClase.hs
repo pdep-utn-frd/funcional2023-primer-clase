cdc86590478a8fd1cd650580c5924af5144c943a
Juan Bono <juanbono94@gmail.com>
2023-05-04 20:27:12 -0300

    Commit primer clase

---
+++ .stack-work/dist/x86_64-linux/Cabal-3.6.3.0/build/primerClase/autogen/Paths_primerClase.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module Paths_primerClase (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+
+import qualified Control.Exception as Exception
+import qualified Data.List as List
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir     = "/home/juan/facultad/pdp2023/primerClase/.stack-work/install/x86_64-linux/f76517a9c05d121525042becdc8698562458eb0b6f51152467669130bffeeeec/9.2.7/bin"
+libdir     = "/home/juan/facultad/pdp2023/primerClase/.stack-work/install/x86_64-linux/f76517a9c05d121525042becdc8698562458eb0b6f51152467669130bffeeeec/9.2.7/lib/x86_64-linux-ghc-9.2.7/primerClase-0.1.0.0-CnRkkz1n0leFYc6uZZvffL-primerClase"
+dynlibdir  = "/home/juan/facultad/pdp2023/primerClase/.stack-work/install/x86_64-linux/f76517a9c05d121525042becdc8698562458eb0b6f51152467669130bffeeeec/9.2.7/lib/x86_64-linux-ghc-9.2.7"
+datadir    = "/home/juan/facultad/pdp2023/primerClase/.stack-work/install/x86_64-linux/f76517a9c05d121525042becdc8698562458eb0b6f51152467669130bffeeeec/9.2.7/share/x86_64-linux-ghc-9.2.7/primerClase-0.1.0.0"
+libexecdir = "/home/juan/facultad/pdp2023/primerClase/.stack-work/install/x86_64-linux/f76517a9c05d121525042becdc8698562458eb0b6f51152467669130bffeeeec/9.2.7/libexec/x86_64-linux-ghc-9.2.7/primerClase-0.1.0.0"
+sysconfdir = "/home/juan/facultad/pdp2023/primerClase/.stack-work/install/x86_64-linux/f76517a9c05d121525042becdc8698562458eb0b6f51152467669130bffeeeec/9.2.7/etc"
+
+getBinDir     = catchIO (getEnv "primerClase_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "primerClase_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "primerClase_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "primerClase_datadir")    (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "primerClase_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "primerClase_sysconfdir") (\_ -> return sysconfdir)
+
+
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '/'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/'
---
+++ src/Main.hs
@@ -0,0 +1,64 @@
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+--     1er arg  2do     retorno  
+conjuncion True  True  = True
+conjuncion False _     = False
+conjuncion True  False = False
+
+esPar :: Integer -> Bool
+esPar unNumero = mod unNumero 2 == 0
+
+esImpar :: Integer -> Bool
+esImpar unNumero = not (esPar unNumero)
+
+-- Pattern Matching
+esVocal :: Char -> Bool
+esVocal unaLetra = False
+esVocal 'a'      = True
+esVocal 'e'      = True
+esVocal 'i'      = True
+esVocal 'o'      = True
+esVocal 'u'      = True
+
+
+
+f 2 = 2
+f x = x * x
+
+palabra :: String
+palabra = "hola"
+
+palabra2 :: String
+palabra2 = " como estas?"
+
+palabra3 :: [Char]
+palabra3 = palabra ++ palabra2
+
+-- Cosas que se pueden hacer con un string:
+-- - concatenar  (++)
+-- - take y drop 
+-- - 
+
+
+reemplazar :: String -> String -> String
+reemplazar reemplazo _ = reemplazo ++ "hola"
+
+
+sumarTupla :: (Bool, Bool, Bool) -> Bool
+sumarTupla (x, y, z) = x && (not y) || x && z
+
+
+-- suma 2 vectores en R2
+sumarVectores :: (Float, Float) -> (Float, Float) -> (Float, Float)
+sumarVectores primerTupla segundaTupla = (primerComponente, segundaComponente)
+    where
+    primerComponente  = fst primerTupla + fst segundaTupla
+    segundaComponente = snd primerTupla + snd segundaTupla
+concatenarListas l1 l2 = l1 ++ l2
+concatenarTupla :: (a, b) -> (c, d) -> (a, b, c, d)
+concatenarTupla (x, y) (w, z) = (x, y , w , z)
+
+primerYUltimaLetra :: String -> (Char, Char)
+primerYUltimaLetra palabra = (head palabra, last palabra)
